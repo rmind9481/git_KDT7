50dbc56394ae3a28d7d7803c2bea5b43c9ce578d
rmind9481 <rmind9481@gmail.com>
2025-02-27 00:06:35 +0900

    lambda study

--- 01.PYTHON/00.Python_Study/python.py
+++ 01.PYTHON/00.Python_Study/python.py
@@ -5,17 +5,89 @@
 
 add = lambda x,y : x+y 
 
-print(add(3,8))
-print((lambda x, y: x + y)(3, 5))
+#print(add(3,8))
+
 
 # 2. lambda 함수를 사용하여 주어진 숫자의 제곱을 반환하는 함수를 작성하세요.
-# 3. 세 개의 숫자를 입력받아 그중 가장 큰 값을 반환하는 lambda 함수를 작성하세요.
+
+a = lambda x : x**2
+
+print(f"제곱: {a(3)}")
+
+
+###
+# 3. 세 개의 숫자를 입력받아 그중 가장 큰 값을 반환하는 lambda 함수를 작성하세요
+
+three_nums = lambda x,y,z : max((x,y,z))
+
+print(three_nums(3,5,15))
+
+###
+
+print((lambda x, y: x + y)(3, 5))
+
+def get_max(x,y,z):
+	return max(x,y,z)
+
+print(get_max(5,10,35))
+
+
+###
+
+def get_max2(x,y,z):
+	if x >= y and x >= z:
+		return x
+	elif y>= x and y >=z:
+		return y
+	else:
+		return z
+
+print(get_max2(50,3,7))
+
+
 # 4. 문자열을 입력받아 해당 문자열의 길이를 반환하는 lambda 함수를 작성하세요.
+
+str_input = lambda str_msg : len(str_msg)
+
+
+print( str_input('파이썬 잘하고싶다.!!'))
+
 # 5. 섭씨 온도를 입력하면 화씨 온도로 변환하는 lambda 함수를 작성하세요.
+
+# [°F] = [°C] × 1.8 + 32
+
+dgree = lambda x : (x*1.8)+32
+
+print(f"섭씨 50  = 화씨 { dgree(50)}")
+
+
+###
 # 6. 리스트 [3, 1, 4, 1, 5, 9, 2]을 lambda와 sorted()를 이용하여 오름차순 정렬하세요.
+
+list_b = [3,1,4,1,5,9,2] 
+
+
+a_list = lambda x : sorted(x)
+print(a_list(list_b))
+
 # 7. 두 개의 리스트가 주어졌을 때, 같은 인덱스끼리 곱한 결과를 새로운 리스트로 반환하는 lambda 함수를 작성하세요.
+
+a_list = [1,2,3,4,5]
+b_list  =[11,22,33,44,55]
+
+Double_list = lambda x,y : [x[i]*y[i] for i in range(len(x)) ]
+
+print(Double_list(a_list,b_list))
+
+
+
+
+
+
 # 8. 주어진 숫자가 짝수인지 홀수인지 판단하는 lambda 함수를 작성하세요.
+
 # 9. 주어진 리스트에서 5보다 큰 숫자들만 필터링하는 lambda 함수를 작성하세요.
+
 # 10. 문자열 리스트를 받아 문자열 길이 기준으로 정렬하는 lambda 함수를 작성하세요.
 
 
